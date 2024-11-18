69d66ec9e188205ac134b45f16c36491c34d40b6
Abraham Sinsard <abrahamsinsard@MBP-de-Abraham.lan>
2024-11-18 23:40:29 +0100

    update itoa + striteri and add lstdelone

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,55 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asinsard <asinsard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: abrahamsinsard <abrahamsinsard@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:09:57 by asinsard          #+#    #+#             */
-/*   Updated: 2024/11/18 19:22:13 by asinsard         ###   ########lyon.fr   */
+/*   Updated: 2024/11/18 23:07:23 by abrahamsins      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_intlen(int n)
+size_t	ft_intlen(long int res)
 {
-	size_t	count;
+	int	count;
+	int	neg;
 
 	count = 1;
-	while (n >= 10)
+	neg = 1;
+	if (res < 0)
+		neg *= -1;
+	res *= neg;
+	while (res >= 10)
 	{
-		n /= 10;
+		res /= 10;
 		count++;
 	}
 	return (count);
 }
 
+char	*ft_intis(long int res, int len, char *str)
+{
+	if (res < 0)
+	{
+		while (res != 0)
+		{
+			str[len] = 48 - (res % 10);
+			res /= 10;
+			len--;
+		}
+		str[len] = '-';
+	}
+	if (res > 0)
+	{
+		while (res != 0)
+		{
+			str[len] = (res % 10) + 48;
+			res /= 10;
+			len--;
+		}
+	}
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
-	unsigned char	*str;
-	int				len;
+	char		*str;
+	int			len;
+	long int	res;
 
-	len = ft_intlen(n);
-	str = malloc(sizeof(char *) * len + 1);
+	res = n;
+	len = ft_intlen(res);
+	if (res < 0)
+	{
+		len += 1;
+		str = malloc(sizeof(char *) * len + 1);
+	}
+	if (res >= 0)
+		str = malloc(sizeof(char *) * len + 1);
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
 	len--;
-	if (n == 0)
+	if (res == 0)
 	{
 		str[len] = 48;
 		return ((char *)str);
 	}
-	while (n != 0)
-	{
-		str[len] = (n % 10) + 48;
-		n /= 10;
-		len--;
-	}
-	return ((char *)str);
+	ft_intis(res, len, str);
+	return (str);
 }
 
 /*int	main(void)
 {
-	printf("%s", ft_itoa(-555));
+	printf("%s", ft_itoa(1000034));
 	return (0);
 }*/
--- ft_striteri.c
+++ ft_striteri.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asinsard <asinsard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: abrahamsinsard <abrahamsinsard@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 19:25:11 by asinsard          #+#    #+#             */
-/*   Updated: 2024/11/18 19:26:08 by asinsard         ###   ########lyon.fr   */
+/*   Updated: 2024/11/18 23:34:53 by abrahamsins      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}	
 }
+
