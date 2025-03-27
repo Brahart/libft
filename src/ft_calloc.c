4148918bbd245caba2e31957bd11d345e275d73c
Abraham Sinsard <asinsard@z2r2p7.42lyon.fr>
2025-03-27 02:30:23 +0100

    secure ft_calloc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: asinsard <asinsard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:52:27 by asinsard          #+#    #+#             */
-/*   Updated: 2025/03/26 00:26:22 by asinsard         ###   ########lyon.fr   */
+/*   Updated: 2025/03/27 02:29:58 by asinsard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	i;
 	void	*tab;
+	void	*zero;
 
 	i = 0;
 	if (nmemb == 0 || size == 0)
-		return (malloc(0));
+	{
+		zero = malloc(0);
+		if (!zero)
+			return (NULL);
+		return (zero);
+	}
 	if (size != 0 && nmemb > 2147483647 / size)
 		return (NULL);
 	tab = malloc(size * nmemb);
